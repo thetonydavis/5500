551d656c2061351d4ee168289c0616a5a4acabee
thetonydavis <31522252+thetonydavis@users.noreply.github.com>
2024-05-22 22:38:06 -0700

    Fix routes and ensure correct port for Flask app

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ def search_by_plan_name():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
